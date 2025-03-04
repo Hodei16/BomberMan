004657860c9474d69a32c661d2e8b417b00afe21
Hodei16 <hodei.castro@gmail.com>
2025-03-04 14:30:08 +0100

    Importak

--- Bomberman/src/Bista/Eszenarioa.java
+++ Bomberman/src/Bista/Eszenarioa.java
@@ -8,7 +8,7 @@ import Eredua.Blokea;
 import Eredua.BomberZuria;
 import Eredua.Bonba;
 import Eredua.Gelaxka;
-import Eredua.Gogorra;
+import Eredua.Gogorra; 
 import Eredua.Jokalaria; 
 import Eredua.Pertsonaia;
 
