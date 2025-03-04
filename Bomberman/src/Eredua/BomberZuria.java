5cb6b284e7049dc9aa19a56d7d63773bfe1ea75a
Hodei16 <hodei.castro@gmail.com>
2025-03-04 18:10:36 +0100

    listaBonba

--- Bomberman/src/Eredua/BomberZuria.java
+++ Bomberman/src/Eredua/BomberZuria.java
@@ -1,5 +1,10 @@
 package Eredua;
+import java.util.ArrayList;
 
 public class BomberZuria {
-
+	private ArrayList <Bonba> listaBonba;
+	
+	public BomberZuria() {
+		this.listaBonba = new ArrayList <Bonba>();
+	}
 }
