6086c4e8ec8c116eca85abfdafb813ecd4ec0825
Hodei16 <hodei.castro@gmail.com>
2025-03-04 14:28:11 +0100

    importak

--- Bomberman/src/Bista/Eszenarioa.java
+++ Bomberman/src/Bista/Eszenarioa.java
@@ -1,5 +1,17 @@
 package Bista;
+import java.util.Observable;
+import java.util.Observer;
+import javax.swing.JFrame;
 
-public class Eszenarioa {
+import Eredua.Biguna;
+import Eredua.Blokea;
+import Eredua.BomberZuria;
+import Eredua.Bonba;
+import Eredua.Gelaxka;
+import Eredua.Gogorra;
+import Eredua.Jokalaria;
+import Eredua.Pertsonaia;
 
+public class Eszenarioa extends JFrame implements Observer {
+	
 }
