65adf928aefcb0a5200dc354ab815a02b5459700
Li3R <llaiseka001@ikasle.ehu.eus>
2025-03-04 13:47:16 +0100

    HasieraPantaila-ren EMA egin

--- Bomberman/src/Bista/HasieraPantaila.java
+++ Bomberman/src/Bista/HasieraPantaila.java
@@ -1,5 +1,16 @@
 package Bista;
 
 public class HasieraPantaila {
-
+	private static HasieraPantaila nireHasieraPantaila;
+	
+	private HasieraPantaila() {}
+	
+	public static synchronised HasieraPantaila getNireHasieraPantaila() {
+		if(nireHasieraPantaila == null) {
+			nireHasieraPantaila = new HasieraPantaila();
+		}
+		return nireHasieraPantaila;
+	}
+	
+	
 }
