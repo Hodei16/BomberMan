2ca3658b2011505437c53346bc4d9aa042f14d23
Li3R <llaiseka001@ikasle.ehu.eus>
2025-03-04 16:55:36 +0100

    Gelaxka eguneratu

--- Bomberman/src/Eredua/Gelaxka.java
+++ Bomberman/src/Eredua/Gelaxka.java
@@ -2,9 +2,13 @@ package Eredua;
 
 public class Gelaxka {
 	private BomberZuria bz;
+	private Biguna bb;
+	private Gogorra bg;
 	
-	public Gelaxka(BomberZuria pBz) {
+	public Gelaxka(BomberZuria pBz, Biguna pBb, Gogorra pBg) {
 		this.bz = pBz;
+		this.bb = pBb;
+		this.bg = pBg;
 	}
 	
 	
