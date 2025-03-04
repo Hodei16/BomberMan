22fea9f5bf440cda7dad3dc59a114bfc72de803d
Hodei16 <hodei.castro@gmail.com>
2025-03-04 14:46:30 +0100

    Eszenarioa estruktura

--- Bomberman/src/Bista/Eszenarioa.java
+++ Bomberman/src/Bista/Eszenarioa.java
@@ -13,5 +13,26 @@ import Eredua.Jokalaria;
 import Eredua.Pertsonaia; 
 
 public class Eszenarioa extends JFrame implements Observer {
+	private Gelaxka[][] gelaxkaMatrix;
+	private static Eszenarioa nEszenarioa = null;
 	
+	private Eszenarioa() {
+		this.gelaxkaMatrix = new Gelaxka[10][16];
+		
+	}
+	public static Eszenarioa getEszenarioa() {
+		if (nEszenarioa==null) {
+			nEszenarioa= new Eszenarioa();
+		}
+		return nEszenarioa;
+	}
+	
+	
+	
+	
+	
+	@Override
+	public void update (Observable o, Object arg) {
+		
+	}
 }
