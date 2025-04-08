f113979c7d96f27d5f0aefb438831adec87057d6
Hodei16 <hodei.castro@gmail.com>
2025-04-08 16:47:18 +0200

    Strategy patroia egiteko bomberman abstraktua izango da orain

---
+++ Bomberman/src/Eredua/BomberMan.java
@@ -0,0 +1,60 @@
+package Eredua;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public abstract class BomberMan {
+	private int bonbaKop=10;
+	private int posX;
+	private int posY;
+	
+	public BomberMan (int pPosX, int pPosY) {
+		this.posX = pPosX;
+		this.posY = pPosY;
+	}
+	
+	public void setPosX(int pPosX) {
+		this.posX = pPosX;
+	}
+	public void setPosY(int pPosY) {
+		this.posY = pPosY;
+	}
+	public int getPosX() {
+		return this.posX;
+	}
+	public int getPosY() {
+		return this.posY;
+	}
+	public boolean bonbaDauka() {
+		return bonbaKop!=0;
+	}
+	
+	private static final int PERIODO = 4;
+	private Timer timer = null;
+	private int kont;
+	
+	public void bonbaKendu() {
+		bonbaKop--;
+		if(bonbaKop==0) {
+			kont = PERIODO;
+			TimerTask timerTask = new TimerTask() {
+				@Override
+				public void run() {
+					updateKont();
+				}
+			};
+			timer = new Timer();
+			timer.scheduleAtFixedRate(timerTask, 0, 1000);}
+		}
+		
+		private void updateKont() {
+			kont--;
+			if(kont == 0) {
+				timer.cancel();
+				bonbaKop++;
+			}
+		}
+		
+}
+
+
--- Bomberman/src/Eredua/BomberZuria.java
+++ Bomberman/src/Eredua/BomberZuria.java
@@ -3,57 +3,16 @@ import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class BomberZuria {
+public class BomberZuria extends BomberMan{
 	private int bonbaKop=10;
 	private int posX;
 	private int posY;
 	
 	public BomberZuria(int pPosX, int pPosY) {
-		this.posX = pPosX;
-		this.posY = pPosY;
+		super (pPosX, pPosY);
 	}
 	
-	public void setPosX(int pPosX) {
-		this.posX = pPosX;
-	}
-	public void setPosY(int pPosY) {
-		this.posY = pPosY;
-	}
-	public int getPosX() {
-		return this.posX;
-	}
-	public int getPosY() {
-		return this.posY;
-	}
-	public boolean bonbaDauka() {
-		return bonbaKop!=0;
-	}
-	
-	private static final int PERIODO = 4;
-	private Timer timer = null;
-	private int kont;
-	
-	public void bonbaKendu() {
-		bonbaKop--;
-		if(bonbaKop==0) {
-			kont = PERIODO;
-			TimerTask timerTask = new TimerTask() {
-				@Override
-				public void run() {
-					updateKont();
-				}
-			};
-			timer = new Timer();
-			timer.scheduleAtFixedRate(timerTask, 0, 1000);}
-		}
-		
-		private void updateKont() {
-			kont--;
-			if(kont == 0) {
-				timer.cancel();
-				bonbaKop++;
-			}
-		}
+
 		
 }
 
